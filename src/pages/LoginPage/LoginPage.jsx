79ac98950aa0cd1206b60af7e4691cc2b97fa783
EvgeniyZaretskiy <zaretskiyen@gmail.com>
2025-05-04 18:36:55 +0300

    fix

--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,54 +1,51 @@
 import Header from "../../components/Header/Header.jsx";
 import { routesPath } from "../../lib/routesPath.js";
 import * as S from "./LoginPage.styled.js";
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 //import { loginAuth } from "../../services/Api.js";
 
 export const LoginPage = ({ login }) => {
   return (
     <>
-      <Header/>
+      <Header />
       <S.Wrapper>
-          <S.Modal>
-            <S.ModalBlock>
-              <S.ModalTtl>
-                <h2>Вход</h2>
-              </S.ModalTtl>
-              <S.ModalFormLogin id="formLogIn" action="#">
-                <S.ModalInput
-                  type="text"
-                  name="login"
-                  id="formlogin"
-                  placeholder="Эл. почта"
-                />
-                <S.ModalInput
-                  type="password"
-                  name="password"
-                  id="formpassword"
-                  placeholder="Пароль"
-                />
-                <S.ModalBtnEnter id="btnEnter">
-                  <S.ModalBtnEnterA
-                    onClick={login}
-                    //  logout={login}
-                  >
-                    Войти
-                  </S.ModalBtnEnterA>
-                </S.ModalBtnEnter>
-                <S.ModalFormGroup>
-                  <S.ModalFormGroupAP>
-                    Нужно зарегистрироваться?
-                  </S.ModalFormGroupAP>
-                  <Link to={routesPath.REGISTER}>
-                    <S.ModalFormGroupAP>
-                      Регистрируйтесь здесь
-                    </S.ModalFormGroupAP>
-                  </Link>
-                </S.ModalFormGroup>
-              </S.ModalFormLogin>
-            </S.ModalBlock>
-          </S.Modal>
-        
+        <S.Modal>
+          <S.ModalBlock>
+            <S.ModalTtl>
+              <h2>Вход</h2>
+            </S.ModalTtl>
+            <S.ModalFormLogin id="formLogIn" action="#">
+              <S.ModalInput
+                type="text"
+                name="login"
+                id="formlogin"
+                placeholder="Эл. почта"
+              />
+              <S.ModalInput
+                type="password"
+                name="password"
+                id="formpassword"
+                placeholder="Пароль"
+              />
+              <S.ModalBtnEnter id="btnEnter">
+                <S.ModalBtnEnterA
+                  onClick={login}
+                  //  logout={login}
+                >
+                  Войти
+                </S.ModalBtnEnterA>
+              </S.ModalBtnEnter>
+              <S.ModalFormGroup>
+                <S.ModalFormGroupAP>
+                  Нужно зарегистрироваться?
+                </S.ModalFormGroupAP>
+                <Link to={routesPath.REGISTER}>
+                  <S.ModalFormGroupAP>Регистрируйтесь здесь</S.ModalFormGroupAP>
+                </Link>
+              </S.ModalFormGroup>
+            </S.ModalFormLogin>
+          </S.ModalBlock>
+        </S.Modal>
       </S.Wrapper>
     </>
   );
--- src/pages/RegisterPage/RegisterPage.jsx
+++ src/pages/RegisterPage/RegisterPage.jsx
@@ -1,4 +1,4 @@
-import { Link } from "react-router-dom";
+import { Link } from "react-router";
 import Header from "../../components/Header/Header.jsx";
 import * as S from "./RegisterPage.styled.js";
 import { routesPath } from "../../lib/routesPath.js";
@@ -6,50 +6,50 @@ import { routesPath } from "../../lib/routesPath.js";
 export const RegisterPage = () => {
   return (
     <>
-      <Header/>
-        <S.Wrapper>
-            <S.Modal>
-              <S.ModalBlock>
-                <S.ModalTtl>
-                  <h2>Регистрация</h2>
-                </S.ModalTtl>
-                <S.ModalFormLogin id="formLogUp" action="#">
-                  <S.ModalInput
-                    type="text"
-                    name="first-name"
-                    id="first-name"
-                    placeholder="Имя"
-                  />
-                  <S.ModalInput
-                    type="text"
-                    name="login"
-                    id="loginReg"
-                    placeholder="Эл. почта"
-                  />
-                  <S.ModalInput
-                    type="password"
-                    name="password"
-                    id="passwordFirst"
-                    placeholder="Пароль"
-                  />
-                  <Link to={routesPath.LOGIN}>
-                    <S.ModalBtnRegisterEnter id="RegistrEnter">
-                      <S.ModalBtnRegisterEnterA>
-                        {" "}
-                        Зарегистрироваться
-                      </S.ModalBtnRegisterEnterA>
-                    </S.ModalBtnRegisterEnter>
-                  </Link>
-                  <S.ModalFormGroup>
-                    <S.ModalFormGroupAP>Уже есть аккаунт?</S.ModalFormGroupAP>
-                    <Link to={routesPath.LOGIN}>
-                      <S.ModalFormGroupAP>Войдите здесь</S.ModalFormGroupAP>
-                    </Link>
-                  </S.ModalFormGroup>
-                </S.ModalFormLogin>
-              </S.ModalBlock>
-            </S.Modal>
-        </S.Wrapper>
+      <Header />
+      <S.Wrapper>
+        <S.Modal>
+          <S.ModalBlock>
+            <S.ModalTtl>
+              <h2>Регистрация</h2>
+            </S.ModalTtl>
+            <S.ModalFormLogin id="formLogUp" action="#">
+              <S.ModalInput
+                type="text"
+                name="first-name"
+                id="first-name"
+                placeholder="Имя"
+              />
+              <S.ModalInput
+                type="text"
+                name="login"
+                id="loginReg"
+                placeholder="Эл. почта"
+              />
+              <S.ModalInput
+                type="password"
+                name="password"
+                id="passwordFirst"
+                placeholder="Пароль"
+              />
+              <Link to={routesPath.LOGIN}>
+                <S.ModalBtnRegisterEnter id="RegistrEnter">
+                  <S.ModalBtnRegisterEnterA>
+                    {" "}
+                    Зарегистрироваться
+                  </S.ModalBtnRegisterEnterA>
+                </S.ModalBtnRegisterEnter>
+              </Link>
+              <S.ModalFormGroup>
+                <S.ModalFormGroupAP>Уже есть аккаунт?</S.ModalFormGroupAP>
+                <Link to={routesPath.LOGIN}>
+                  <S.ModalFormGroupAP>Войдите здесь</S.ModalFormGroupAP>
+                </Link>
+              </S.ModalFormGroup>
+            </S.ModalFormLogin>
+          </S.ModalBlock>
+        </S.Modal>
+      </S.Wrapper>
     </>
   );
 };
